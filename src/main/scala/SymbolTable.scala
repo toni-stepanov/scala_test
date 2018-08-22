c99bfecd76a2ce8c1b127e3bbde77cd53c3c968d
Anton Stepanov <anton.stepanov@firstlinesoftware.com>
2018-08-22 13:57:34 +0300

    add bst

---
+++ src/main/scala/SymbolTable.scala
@@ -0,0 +1,13 @@
+trait SymbolTable[K, V] {
+  def put(k: K, v: V): Unit  // Add an entry
+  def apply(k: K): Option[V] // Get an entry
+  def clear(): Unit // Empty table
+  def contains(k: K): Boolean = this(k) match {
+    case Some(_) => true
+    case None => false
+  }// Is key present?
+  def delete(k: K): Unit  // Delete a key
+  def foreach(f: (K, V) => Unit): Unit  // Iterate over table
+  def isEmpty: Boolean = this.size == 0
+  def size: Int
+}
